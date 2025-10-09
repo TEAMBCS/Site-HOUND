3ecf75d97528d543000ef5c184586d5335d96472
Bangladesh Cyber Squad <147122723+TEAMBCS@users.noreply.github.com>
2025-10-09 23:57:48 +0600

    Delete site_hound.py

--- site_hound.py
+++
@@ -1,7 +0,0 @@
-# এই দাদু এটা কী দেখো এইখানে, এটা change করে লাভ নাই 🙂
-# চায়লে এটাতে নিজের script add করতে পারো problem নাই ❤️‍🩹
-import os
-import time
-print("\033[96m\n\n  ➤ \033[92m SITE HOUND started ")
-time.sleep(1.3)
-os.system("python3 site_hound")
